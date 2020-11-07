7b1c74ef4afa4a6df69ad2a76a312839059e1f53
Ville Lappalainen <ville.lappalainen@helsinki.fi>
2020-11-07 17:36:52 +0200

    Kakkosviikon laskarit

---
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
@@ -0,0 +1,24 @@
+package ohtu.verkkokauppa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Pankki implements PankkiInterface {
+
+    @Autowired
+    private KirjanpitoInterface kirjanpito;
+
+//    public Pankki(KirjanpitoInterface pito) {
+//        this.kirjanpito = pito;
+//    }
+
+    @Override
+    public boolean tilisiirto(String nimi, int viitenumero, String tililta, String tilille, int summa) {
+        kirjanpito.lisaaTapahtuma("tilisiirto: tililtä " + tilille + " tilille " + tilille
+                + " viite " + viitenumero + " summa " + summa + "e");
+
+        // täällä olisi koodi joka ottaa yhteyden pankin verkkorajapintaan
+        return true;
+    }
+}
